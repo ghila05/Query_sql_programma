ecbad0275a71e85600f2a8dad30499815113b685
Ghilardi.19320 <Ghilardi.19320@LABI1-6PC12>
2024-01-10 11:38:59 +0100

    Aggiungere i file di progetto.

---
+++ Query_sql_programma/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Drawing;
+
+namespace Query_sql_programma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=Nicola;pwd=12345;database=film";
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+            conn.Open();
+
+
+            String sql1 = "select * from ifilm;";
+            MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+            cmd1.ExecuteNonQuery();
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = cmd1;
+            DataTable dati = new DataTable();
+            MyAdapter.Fill(dati);
+            dataGridView1.DataSource = dati;
+            conn.Close();
+
+
+
+        }
+
+    }
+}
