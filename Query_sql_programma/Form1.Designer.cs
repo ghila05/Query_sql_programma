892954ce4312d61ac1f223180f551713f3763a04
ghila05 <nicolaghi05@gmail.com>
2024-01-12 21:50:47 +0100

    aggiunta alcuni filtri a scaffali

--- Query_sql_programma/Form1.Designer.cs
+++ Query_sql_programma/Form1.Designer.cs
@@ -30,81 +30,177 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Oggetti = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Magazzino = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.comboBox_id_scaff = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.Oggetti.SuspendLayout();
+            this.Magazzino.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Location = new System.Drawing.Point(354, 7);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(756, 368);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(662, 453);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Controls.Add(this.Oggetti);
+            this.tabControl1.Controls.Add(this.Magazzino);
+            this.tabControl1.Location = new System.Drawing.Point(16, 15);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(776, 406);
+            this.tabControl1.Size = new System.Drawing.Size(1035, 500);
             this.tabControl1.TabIndex = 1;
             this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
-            // tabPage1
+            // Oggetti
             // 
-            this.tabPage1.Controls.Add(this.dataGridView1);
-            this.tabPage1.ForeColor = System.Drawing.Color.Black;
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(768, 380);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Sale";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            this.Oggetti.Controls.Add(this.button2);
+            this.Oggetti.Controls.Add(this.comboBox2);
+            this.Oggetti.Controls.Add(this.label2);
+            this.Oggetti.Controls.Add(this.dataGridView1);
+            this.Oggetti.ForeColor = System.Drawing.Color.Black;
+            this.Oggetti.Location = new System.Drawing.Point(4, 25);
+            this.Oggetti.Margin = new System.Windows.Forms.Padding(4);
+            this.Oggetti.Name = "Oggetti";
+            this.Oggetti.Padding = new System.Windows.Forms.Padding(4);
+            this.Oggetti.Size = new System.Drawing.Size(1027, 471);
+            this.Oggetti.TabIndex = 0;
+            this.Oggetti.Text = "Oggetti";
+            this.Oggetti.UseVisualStyleBackColor = true;
+            this.Oggetti.Click += new System.EventHandler(this.tabPage1_Click);
             // 
-            // tabPage2
+            // button2
             // 
-            this.tabPage2.Controls.Add(this.dataGridView2);
-            this.tabPage2.ForeColor = System.Drawing.Color.Black;
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(768, 380);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Cinema";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(12, 429);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 31);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "applica filtri ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(111, 11);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Filtra per: ";
+            // 
+            // Magazzino
+            // 
+            this.Magazzino.Controls.Add(this.comboBox_id_scaff);
+            this.Magazzino.Controls.Add(this.button1);
+            this.Magazzino.Controls.Add(this.comboBox1);
+            this.Magazzino.Controls.Add(this.label1);
+            this.Magazzino.Controls.Add(this.dataGridView2);
+            this.Magazzino.ForeColor = System.Drawing.Color.Black;
+            this.Magazzino.Location = new System.Drawing.Point(4, 25);
+            this.Magazzino.Margin = new System.Windows.Forms.Padding(4);
+            this.Magazzino.Name = "Magazzino";
+            this.Magazzino.Padding = new System.Windows.Forms.Padding(4);
+            this.Magazzino.Size = new System.Drawing.Size(1027, 471);
+            this.Magazzino.TabIndex = 1;
+            this.Magazzino.Text = "Scaffali";
+            this.Magazzino.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "applica filtri ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "id",
+            "categoria"});
+            this.comboBox1.Location = new System.Drawing.Point(111, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Filtra per: ";
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView2.Location = new System.Drawing.Point(353, 7);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(756, 368);
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(663, 453);
             this.dataGridView2.TabIndex = 0;
+            // 
+            // comboBox_id_scaff
+            // 
+            this.comboBox_id_scaff.FormattingEnabled = true;
+            this.comboBox_id_scaff.Items.AddRange(new object[] {
+            "crescente",
+            "decrescente"});
+            this.comboBox_id_scaff.Location = new System.Drawing.Point(111, 72);
+            this.comboBox_id_scaff.Name = "comboBox_id_scaff";
+            this.comboBox_id_scaff.Size = new System.Drawing.Size(121, 24);
+            this.comboBox_id_scaff.TabIndex = 4;
+            this.comboBox_id_scaff.Visible = false;
             // 
             // Database_film
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Database_film";
-            this.Text = "Database_film";
+            this.Text = "Magazzino";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
+            this.Oggetti.ResumeLayout(false);
+            this.Oggetti.PerformLayout();
+            this.Magazzino.ResumeLayout(false);
+            this.Magazzino.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
@@ -114,9 +210,16 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage Oggetti;
+        private System.Windows.Forms.TabPage Magazzino;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox_id_scaff;
     }
 }
 
--- Query_sql_programma/Form1.cs
+++ Query_sql_programma/Form1.cs
@@ -11,6 +11,7 @@ using MySql.Data.MySqlClient;
 using System.Data;
 using System.Drawing;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
 
 namespace Query_sql_programma
 {
@@ -23,36 +24,9 @@ namespace Query_sql_programma
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-
             dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             tabControl1.SelectedIndex = 2;
-            /*
-            
-            // impostazioni datagridview 
-            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-
-
-
-            // connessione 
-            String ConnectionString = "server=127.0.0.1;uid=Nicola;pwd=12345;database=film";
-            MySqlConnection conn = new MySqlConnection(ConnectionString);
-            conn.Open();
-
-
-            String sql1 = "select * from ifilm;";
-            MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
-            cmd1.ExecuteNonQuery();
-            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
-            MyAdapter.SelectCommand = cmd1;
-            DataTable dati = new DataTable();
-            MyAdapter.Fill(dati);
-            dataGridView1.DataSource = dati;
-            conn.Close();
-            */
-
-
         }
         private void tabPage1_Click(object sender, EventArgs e)
         {
@@ -62,30 +36,112 @@ namespace Query_sql_programma
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
             // connessione 
-            String ConnectionString = "server=127.0.0.1;uid=Nicola;pwd=12345;database=film";
-            MySqlConnection conn = new MySqlConnection(ConnectionString);
-            conn.Open();
+
+            try
+            {
+                String ConnectionString = "server=127.0.0.1;uid=Nicola;pwd=12345;database=magazzino";
+                MySqlConnection conn = new MySqlConnection(ConnectionString);
+                conn.Open();
+
+                String sql1 = "select * from oggetti;";
+                MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+                cmd1.ExecuteNonQuery();
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd1;
+                DataTable dati = new DataTable();
+                MyAdapter.Fill(dati);
+
+                String sql2 = "select * from scaffali;";
+                cmd1 = new MySqlCommand(sql2, conn);
+                cmd1.ExecuteNonQuery();
+                MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd1;
+                DataTable dati2 = new DataTable();
+                MyAdapter.Fill(dati2);
+
+                dataGridView1.DataSource = dati;
+                dataGridView2.DataSource = dati2;
+                conn.Close();
+            }catch
+            {
+                throw new Exception("Connessione persa");
+            }
 
 
-            String sql1 = "select * from sale;";
-            MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
-            cmd1.ExecuteNonQuery();
-            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
-            MyAdapter.SelectCommand = cmd1;
-            DataTable dati = new DataTable();
-            MyAdapter.Fill(dati);
 
-            String sql2 = "select * from cinema;";
-            cmd1 = new MySqlCommand(sql2, conn);
-            cmd1.ExecuteNonQuery();
-            MyAdapter = new MySqlDataAdapter();
-            MyAdapter.SelectCommand = cmd1;
-            DataTable dati2 = new DataTable();
-            MyAdapter.Fill(dati2);
 
-            dataGridView1.DataSource = dati;
-            dataGridView2.DataSource = dati2;
-            conn.Close();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // applica filtri scaffali
+        {
+            string filtri = comboBox1.SelectedItem.ToString();
+            if(filtri == "id")
+            {
+                string ordine  = comboBox_id_scaff.SelectedItem.ToString();
+                if (ordine == "crescente")
+                {
+                    String ConnectionString = "server=127.0.0.1;uid=Nicola;pwd=12345;database=magazzino";
+                    MySqlConnection conn = new MySqlConnection(ConnectionString);
+                    conn.Open();
+
+                    String sql1 = "SELECT * FROM scaffali ORDER BY scaffali.id ASC";
+                    MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+                    cmd1.ExecuteNonQuery();
+                    MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                    MyAdapter.SelectCommand = cmd1;
+                    DataTable dati = new DataTable();
+                    MyAdapter.Fill(dati);
+                    dataGridView2.DataSource = dati;
+                }
+                else if (ordine == "decrescente")
+                {
+                    String ConnectionString = "server=127.0.0.1;uid=Nicola;pwd=12345;database=magazzino";
+                    MySqlConnection conn = new MySqlConnection(ConnectionString);
+                    conn.Open();
+
+                    String sql1 = "SELECT * FROM scaffali ORDER BY scaffali.id DESC";
+                    MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+                    cmd1.ExecuteNonQuery();
+                    MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                    MyAdapter.SelectCommand = cmd1;
+                    DataTable dati = new DataTable();
+                    MyAdapter.Fill(dati);
+                    dataGridView2.DataSource = dati;
+                }
+                else throw new Exception("seleziona filtri");
+            }
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string selected = comboBox1.SelectedItem.ToString();
+
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    if(selected == "id")
+                    {
+                        comboBox_id_scaff.Visible = true;
+                    }
+                    break;
+                case 1:
+                    if(selected == "categoria")
+                    {
+
+                    }
+                    break;
+
+            }
+        }
+
+
     }
 }
